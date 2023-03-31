d6c64782e978358b3e1d94eaf0fef33a04904b6f
Simon Charbonnier <simoncharbonnier@orange.fr>
2023-03-31 09:37:45 +0200

    Copy

--- app/config/db.php
+++ app/config/db.php
@@ -1,6 +1,9 @@
 <?php
   try {
-    $db = new PDO('mysql:host=localhost;dbname=OCP5;charset=utf8', 'root', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    $db = new PDO('mysql:host=localhost;dbname=OCP5;charset=utf8',
+      'root',
+      '',
+      [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
   }
   catch(Exception $e) {
     die('Erreur : '.$e->getMessage());
