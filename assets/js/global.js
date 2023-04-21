9f4f5d3b44f3c1158dc385aea848b10cba5919a6
Simon Charbonnier <simoncharbonnier@orange.fr>
2023-04-21 12:25:18 +0200

    fix: default active header

--- assets/js/global.js
+++ assets/js/global.js
@@ -4,7 +4,9 @@ const found = url.match(regex);
 
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll('.nav-item').forEach(item => {
-    if (item.getAttribute('controller') === found[1] && item.getAttribute('action') === found[2]) {
+    if (found && item.getAttribute('controller') === found[1] && item.getAttribute('action') === found[2]) {
+      item.classList.add('active');
+    } else if (found === null && item.getAttribute('controller') === 'home' && item.getAttribute('action') === 'index') {
       item.classList.add('active');
     }
   });
