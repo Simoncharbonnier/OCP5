5b430ab7907bddb4fc36d42d5993d56aaee82fd7
Simon Charbonnier <simoncharbonnier@orange.fr>
2023-05-21 16:24:56 +0200

    fix: code style

--- index.php
+++ index.php
@@ -1,28 +1,25 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
-include_once 'app/config/config.php';
+require_once 'app/config/config.php';
 
-if (!empty($_GET['controller'])) {
-    $controller = $_GET['controller'];
+if (isset($_GET['controller']) === TRUE && empty($_GET['controller']) === FALSE) {
+    $controller = stripslashes($_GET['controller']);
 } else {
     $controller = 'Home';
 }
 
 $controllerClassName = ucfirst($controller);
-if (file_exists('app/controllers/'.$controllerClassName.'.php')) {
-    require_once 'app/controllers/'.$controllerClassName.'.php';
+if (file_exists('app/controllers/'.$controllerClassName.'.php') === TRUE) {
+    include_once 'app/controllers/'.$controllerClassName.'.php';
     $instance = new $controllerClassName();
 
-    if (!empty($_GET['action'])) {
-        $action = $_GET['action'];
+    if (isset($_GET['action']) === TRUE && empty($_GET['action']) === FALSE) {
+        $action = stripslashes($_GET['action']);
     } else {
         $action = 'index';
     }
 
-    if (!method_exists($instance, $action)) {
+    if (method_exists($instance, $action) === FALSE) {
         header("Location: ".PATH."?controller=home&action=index&error=inval");
         exit;
     }
@@ -31,11 +28,11 @@ if (file_exists('app/controllers/'.$controllerClassName.'.php')) {
     exit;
 }
 
-if (!isset($_SESSION)) {
+if (isset($_SESSION) === FALSE) {
     session_start();
-    if (!isset($_SESSION['is_logged'])) {
+    if (isset($_SESSION['is_logged']) === FALSE) {
         $_SESSION['is_logged'] = false;
     }
 }
 
-include_once 'app/views/layout/index.php';
+require_once 'app/views/layout/index.php';
